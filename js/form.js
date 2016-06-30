8dc7ad5850efa3499b9a8987f6620489d1ce5462
Murray Steele <muz@h-lame.com>
2016-06-30 15:53:14 +0100

    Use numberWithCommas for presenting the population in panel

--- js/form.js
+++ js/form.js
@@ -348,7 +348,7 @@
     $('#constituency_info').append('<p class="mp">' + mpForConstituency.mp + '</p>');
     $('#constituency_info').append('<p class="party">' + mpForConstituency.party + '</p>');
     $('#constituency_info').append('<p class="signatures_count"><span class="data">' + numberWithCommas(count) + '</span> signatures</p>');
-    $('#constituency_info').append('<p class="percentage">' + percentage + "% of " + population + " constituents" + '</p>');
+    $('#constituency_info').append('<p class="percentage">' + percentage + "% of " + numberWithCommas(population) + " constituents" + '</p>');
     if (!ui_hidden) {
       $('#constituency_info').show();
     }
